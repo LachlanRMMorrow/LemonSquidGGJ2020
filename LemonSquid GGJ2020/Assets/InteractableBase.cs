53776b776ca1a413ce39b2ce16fcd7553e2a166a
Palmer96 <kaynepalmer@hotmail.com>
2020-02-01 08:19:28 +1100

    Added InteractableBase script

---
+++ LemonSquid GGJ2020/Assets/InteractableBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableBase : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // TEMP - Used to correct for weird scale on placeholder objects
+        transform.position += Vector3.up * transform.localScale.y/2;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+        // Debug
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Interact();
+        }
+    }
+
+
+
+    public virtual void Interact()
+    {
+        Debug.Log("Break");    
+    }
+}
---
+++ LemonSquid GGJ2020/Assets/InteractableManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableManager : MonoBehaviour
+{
+    public string playerName = "Palmer";
+    public int seed = 0;
+
+    [SerializeField] private InteractableBase[] interactables;
+    [SerializeField] private List<Transform> spawnPoints = new List<Transform>();
+
+    [SerializeField] private int spawnCount;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+       seed = playerName.GetHashCode();
+        Random.seed = seed;
+
+       spawnCount = Mathf.Clamp(spawnCount, 0, spawnPoints.Count);
+
+        for (int i = 0; i < spawnCount; ++i)
+        {
+            Transform locationInfo = GetSpawnPoint();
+
+            if (locationInfo != null)
+            {
+                Instantiate(GetRandomInteractable(), locationInfo.position, locationInfo.rotation);
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    GameObject GetRandomInteractable()
+    {
+        return interactables[Random.Range(0, interactables.Length)].gameObject;
+    }
+
+    Transform GetSpawnPoint()
+    {
+        int num = Random.Range(0, spawnPoints.Count);
+        Transform trn = spawnPoints[num];
+        spawnPoints.RemoveAt(num);
+        return trn;
+
+    }
+
+
+
+
+}
