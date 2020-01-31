f39fa471f968db74c39be1b46b79703057f8f681
Lachlan Morrow <lachlan.rm.morrow@gmail.com>
2020-02-01 09:31:55 +1100

    Imported UIPack

--- LemonSquid GGJ2020/Assets/Scripts/NewBehaviourScript.cs
+++ LemonSquid GGJ2020/Assets/Scripts/NewBehaviourScript.cs
@@ -2,14 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
-    public int MoveSpeed;
-    public AudioClip Footsteps;
-
-
-
-
     // Start is called before the first frame update
     void Start()
     {
